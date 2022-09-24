ff2db8c17b0d52ab0cd45acadf14d559e1b1bf7e
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-09-24 18:18:06 -0400

    Initial commit
    Work done up to this point

---
+++ app.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var serverRouter = require("./routes/server.js");
+
+var app = express();
+app.set("views", "views");
+app.set("view engine", "ejs"); //using ejs as view engine
+app.use(express.json());
+app.use(express.urlencoded({extended:true})); //handling incoming url
+app.use(express.static("public")) //images, stylesheets etc from public, gets it before routes
+
+app.use("/", serverRouter);
+
+app.listen(3000, () => {
+    console.log('Express is running port 3000')
+});
+

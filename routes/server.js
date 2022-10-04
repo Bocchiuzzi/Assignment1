3de1f50fb66ff7bf9eb1da5547f649641d86625c
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-10-04 13:48:13 -0400

    Fixed the issues
    views pathing redone, app.js fixed, favicon is even working

--- routes/server.js
+++ routes/server.js
@@ -4,27 +4,26 @@ var app = express.Router();
 app.get("/", (req, res) => {
     res.render("server", {title: "Express"})
     });
-
+    
 app.get("/server", function(req, res) {
     res.render("server");
 });
 
-app.get("D:/Centennial/Semester 3/WebAppDev/Assignment1/views/about", function(req, res) {
+app.get("/views/about", function(req, res) {
     res.render('about');
 });
 
-app.get("file:/Centennial/Semester 3/WebAppDev/Assignment1/views/projects", function(req, res) {
+app.get("/views/projects", function(req, res) {
     res.render("projects");
 });
 
-app.get("D:/Centennial/Semester 3/WebAppDev/Assignment1/views/services", function(req, res) {
+app.get("/views/services", function(req, res) {
     res.render("services");
 });
 
-app.get("D:/Centennial/Semester 3/WebAppDev/Assignment1/views/contact", function(req, res) {
+app.get("/views/contact", function(req, res) {
     res.render("contact");
 });
-
 
 module.exports = app;
 

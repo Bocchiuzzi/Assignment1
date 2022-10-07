7641a2ae64f3a6f52f8b3b2fa7b9e72806ae98c5
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-10-07 17:06:07 -0400

    still trying to fix heroku

--- routes/server.js
+++ routes/server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("server", {title: "Express"})
+    res.render("server", {title: "Home"})
     });
     
 router.get("/server", function(req, res) {

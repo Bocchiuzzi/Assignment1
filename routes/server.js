bfaea39b038adcbc8621dd33f0b0137373283db8
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-10-07 17:26:20 -0400

    reverted to previous version
    reversion

--- routes/server.js
+++ routes/server.js
@@ -1,29 +1,29 @@
-var express = require("express");
-var app = express.Router();
+var express = require('express');
+var router = express.Router();
 
-app.get("/", (req, res) => {
-    res.render("server", {title: "Express"})
+router.get("/", (req, res) => {
+    res.render("server", {title: "Home"})
     });
     
-app.get("/server", function(req, res) {
-    res.render("server");
+router.get("/server", function(req, res) {
+res.render("server");
 });
 
-app.get("/views/about", function(req, res) {
+router.get("/views/about", function(req, res) {
     res.render('about');
 });
 
-app.get("/views/projects", function(req, res) {
+router.get("/views/projects", function(req, res) {
     res.render("projects");
 });
 
-app.get("/views/services", function(req, res) {
+router.get("/views/services", function(req, res) {
     res.render("services");
 });
 
-app.get("/views/contact", function(req, res) {
+router.get("/views/contact", function(req, res) {
     res.render("contact");
 });
 
-module.exports = app;
+module.exports = router;
 

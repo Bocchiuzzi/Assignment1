7db49c8d79a43e5871dc7491834f2db8d4057136
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-10-07 19:31:55 -0400

    fixed all files
    fixed fixed fixed

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-const express = require('express');
-var favicon = require('serve-favicon');
-const serverRouter = require("./routes/server.js");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 
-const app = express();
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
-app.set("views", "views");
-app.set("view engine", "ejs"); //using ejs as view engine
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true})); //handling incoming url
-app.use(express.static("public")) //images, stylesheets etc from public, gets it before routes
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", serverRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
-app.listen(3000, () => {
-    console.log('Express is running port 3000')
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
 });
 
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('server', { title: 'Express' });
+});
+  
+router.get("/server", function(req, res, next) {
+  res.render("server");
+});
+
+router.get("/views/about", function(req, res, next) {
+  res.render('about');
+});
+
+router.get("/views/projects", function(req, res, next) {
+  res.render("projects");
+});
+
+router.get("/views/services", function(req, res, next) {
+  res.render("services");
+});
+
+router.get("/views/contact", function(req, res, next) {
+  res.render("contact");
+});
+
+module.exports = router;

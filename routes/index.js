d2e73f4e29f54a6d76ebc860cd7e4f3831b33474
Bocchiuzzi <bocchiuzzi@hotmail.com>
2022-10-07 20:01:09 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('server', { title: 'Express' });
+  res.render('index', { title: 'Express' });
 });
   
 router.get("/server", function(req, res, next) {
